743584b4760d58eb6b2638b8a3ddea4449d19fab
Zahra Azarbad <zazarbad77@gmail.com>
2023-04-13 06:27:33 +0430

    complete Q2

--- 1-5/hw06-1_5.js
+++ 1-5/hw06-1_5.js
@@ -1,31 +1,50 @@
 ////////////////1//////////////////
 
 
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250,
-    "David": 300
-};
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250,
+//     "David": 300
+// };
 
 
-function topSalary(personSalary) {
+// function topSalary(personSalary) {
 
-    const topSalaryName = [];
+//     const topSalaryName = [];
 
-    let topSalaryCost = 0;
+//     let topSalaryCost = 0;
 
-    for (const [name, salary] of Object.entries(personSalary)) {
-        if (salary >= topSalaryCost) {
-            topSalaryCost = salary;
-        }
-    }
+//     for (const [name, salary] of Object.entries(personSalary)) {
+//         if (salary >= topSalaryCost) {
+//             topSalaryCost = salary;
+//         }
+//     }
 
-    topSalaryName.push(Object.keys(personSalary).filter(key => personSalary[key] == topSalaryCost));
+//     topSalaryName.push(Object.keys(personSalary).filter(key => personSalary[key] == topSalaryCost));
 
-    return topSalaryName;
-}
+//     return topSalaryName;
+// }
 
 
-const result = topSalary(salaries);
-console.log(result);
+// const result = topSalary(salaries);
+// console.log(result);
+
+
+////////////////////2////////////////////////
+
+// function sortMachine() {
+//     const sortList = [];
+//     [...arguments].forEach((array) => {
+//         sortList.push(...array)
+//     })
+//     return sortList.sort((first, second) => {
+//         return second - first;
+//     });
+// }
+
+// console.log(...sortMachine([2, 3, 4], [1, 2, 15], [0, 58, 123]));
+
+
+
+//////////////////////////////3////////////////////////////////////
